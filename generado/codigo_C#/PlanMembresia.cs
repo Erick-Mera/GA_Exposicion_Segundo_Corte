cb71cf9f2f87ca51bc5b833f01cb8be4487060c6
Dev-rec <hermackjh@gmail.com>
2026-07-21 08:07:25 -0500

    Incorporar código C# generado desde el modelo UML

---
+++ generado/codigo_C#/PlanMembresia.cs
@@ -0,0 +1,48 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class PlanMembresia {
+
+    public PlanMembresia() {
+    }
+
+    private int idPlan;
+
+    private string nombre;
+
+    private int duracionDias;
+
+    private decimal precio;
+
+    private string descripcion;
+
+    private bool activo;
+
+    public HashSet<Membresia> membresias;
+
+    /// <summary>
+    /// @param fechaInicio 
+    /// @return
+    /// </summary>
+    public DateTime calcularFechaFin(DateTime fechaInicio) {
+        // TODO implement here
+        return null;
+    }
+
+    /// <summary>
+    /// @param nuevoPrecio 
+    /// @return
+    /// </summary>
+    public void actualizarPrecio(decimal nuevoPrecio) {
+        // TODO implement here
+        return null;
+    }
+
+    public void Operation3() {
+        // TODO implement here
+    }
+
+}
